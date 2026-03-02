fb5b59a1c09c5c56059c6e5e41ee096b3b979913
Viktor Axén <viktor.axen@gmail.com>
2026-03-02 20:20:24 +0100

    get_models lists from database

--- packages/server/src/mlserver/routes/models.py
+++ packages/server/src/mlserver/routes/models.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
+from sqlmodel import Session, select
 
-import mlserver.config as config
+from mlserver.models.registered_model import RegisteredModel
+from mlserver.state import get_engine
 
 router = APIRouter()
 
 
 @router.get("/models")
-async def get_models():
-    return [file.stem for file in config.models_path.glob("*.onnx")]
+async def get_models() -> list[str]:
+    with Session(get_engine()) as session:
+        models = session.exec(select(RegisteredModel.name)).all()
+
+    return list(models)
